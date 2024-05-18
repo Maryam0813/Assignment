001792bc31f8dbf0063629de3ad16143152e6ea3
Maryam0813 <maryamasim0813@gmail.com>
2024-05-18 13:54:25 +0500

    Add files via upload

---
+++ Maryam_JS_Assignment_(1)_Calculator.js
@@ -0,0 +1,20 @@
+// alert("working");
+
+// Prompt the user for the first number
+let firstNumber = prompt("Enter the first number:");
+// Convert the input to a number and store it
+firstNumber = parseFloat(firstNumber);
+
+// Prompt the user for the second number
+let secondNumber = prompt ("Enter the second number:");
+// Convert the input to a number and store it
+secondNumber = parseFloat(secondNumber);
+
+// Calculate the sum of the two numbers
+let sum = firstNumber + secondNumber;
+
+// Display the sum using an alert
+alert("The sum of " + firstNumber + " and " + secondNumber + " is: " + sum);
+
+
+
